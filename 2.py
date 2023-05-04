c4c534a53a4e98f7922fd2cd1ede4e128cbb7051
Nayem <naeemislam13790@gmail.com>
2023-05-04 18:49:50 +0600

    'commited'

--- 2.py
+++ 2.py
@@ -93,15 +93,118 @@
 
 
 
-list1 = [2,3,6,6,5,8,-2,-666,87]
-lenth = len(list1)
-for i in range(lenth):
-    max_num = i
-    for j in range(i+1,lenth):
-      if list1[j]>list1[max_num]:
-        max_num = j
-    (list1[max_num],list1[i]) = (list1[i],list1[max_num])
+# list1 = [57,57, 57]
+# mx = max(list1)
+# sc = None
+
+# for num in list1:
+#     if num == mx:
+#             pass
+#     elif sc == None:
+#             sc = num
+#     elif num > sc:
+#             sc = num
+# print(sc)
+
+
+
     
-print(list1[max_num])
-print(list1)
+# query_name = input()
+# student_marks = {
+#     'Krishna': [67, 68, 69],
+#     'Arjun': [70, 98, 63],
+#     'Malika': [52, 56, 60]
+# }
+
+# sum = 0
+# for i in student_marks[query_name]:
+#     sum = sum + i
     
+# f = sum/3
+# print("{:0.2f}".format(f))
+
+
+
+# query_name = input()
+# sonuc = 0
+# student_marks = {
+#         "Nayem": [45,55,78],
+#         "Shakib": [55,65,34],
+# }
+
+# for i in student_marks[query_name]:
+#         sonuc+=i
+
+# average = sonuc / len(student_marks[query_name])
+# print("%.2f" %average)
+
+# list1 = [
+    
+#     [48,32,65,34],
+#     [53,65,67,43],
+#     [54,65,76,34]
+# ]
+
+# print(list1[0][0])
+
+
+
+# li = []
+# for i in range(5):
+#     li.append([])
+#     for j in range(5):
+#         li[i].append(j)
+# print(li)
+
+# li = [34,22,12,34,6,43,4,3,3,4]
+# mins = min(li)
+# scl = None
+# for i in li:
+#     if mins == i:
+#         pass
+#     elif scl == None:
+#         scl = i
+#     elif i<scl:
+#         scl = i
+# print(scl)
+    
+# if __name__ == '__main__':
+#     scores = []
+#     for _ in range(int(input())):
+#         name = input()
+#         score = float(input()) 
+#         scores.append([name, score])
+# scores = [['nayem',45],['tamim',56],['sabbir',65],['saiful',9]]
+    
+#     # Find min score
+# min_score = min(scores, key = lambda x: x[1])
+# print(min_score)
+    
+#     # Keep only students with score higher than min
+# scores = [score for score in scores if score[1] > min_score[1]]
+    
+#     # Find second min score
+# second_min_score = min(scores, key = lambda x: x[1])
+    
+#     # Keep only students with second min score
+# scores = [score for score in scores if score[1] == second_min_score[1]]
+    
+#     # Sort students alphabetically
+# scores.sort(key = lambda x: x[0])
+    
+# for score in scores:
+#         print(score[0])
+
+scores = [['nayem',45.56],['tamim',56.64],['sabbir',65.07],['saiful',9.87],['omar',14.54],['saiful',14.54]]
+mini = min(scores, key=lambda y : y[1])
+scm = None
+for i in scores:
+    if mini[1] == i[1]:
+        pass
+    elif scm == None:
+        scm = i
+    elif scm[1]> i[1]:
+        scm = i
+
+ 
+print(scm)
