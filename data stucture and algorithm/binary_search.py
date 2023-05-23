a1797dd0be864d3ca9f9059a487920a6bf2e9cca
Nayem Miah <nayemmiah.cu@gmail.com>
2023-05-24 02:23:22 +0600

    'commited'

---
+++ data stucture and algorithm/binary_search.py
@@ -0,0 +1,10 @@
+n = ['nayem','emran','rashel','rifat','maysha']
+lent = len(n)
+finding = 'maysha '
+
+for i in range(lent):
+    if n[i] == finding:
+        print(n[i])
+    
+    else:
+        print('no result found')
