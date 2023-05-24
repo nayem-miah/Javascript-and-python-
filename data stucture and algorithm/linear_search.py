c9aa86d3dd341db0b6d9f0c8592656ccc45f452d
Nayem Miah <nayemmiah.cu@gmail.com>
2023-05-24 20:33:45 +0600

    'commited'

---
+++ data stucture and algorithm/linear_search.py
@@ -0,0 +1,19 @@
+
+finding = input()
+n = ['nayem','emran','rashel','rifat','Saiful']
+
+k = 0
+for i in range(len(n)):
+    if n[i] == finding:
+        k = 1
+        print("The lenth number is",i,"and the result is",n[i])
+    
+    
+    
+if k == 0:
+    print("Not found")
+
+    
+    
+
+        
